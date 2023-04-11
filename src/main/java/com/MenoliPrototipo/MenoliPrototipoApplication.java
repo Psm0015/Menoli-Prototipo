9a0970d2b1fb1dcab7cb1f2c479233dae95ddc59
pedro-mota <pedro.mota@df.estudante.senai.br>
2023-04-11 20:38:03 -0300

    oi

--- src/main/java/com/MenoliPrototipo/MenoliPrototipoApplication.java
+++ src/main/java/com/MenoliPrototipo/MenoliPrototipoApplication.java
@@ -2,12 +2,18 @@ package com.MenoliPrototipo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MenoliPrototipoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MenoliPrototipoApplication.class, args);
+	}
+	
+	@GetMapping("")
+	public String home() {
+		return "Index.html";
 	}
 
 }
